d72c0610b4bbbca181f1e0867783b9f2204c7bd3
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-03-17 17:18:22 +0900

    Feat: Fracture

---
+++ Source/RacingGame/BrakeAbleGimmick.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BrakeAbleGimmick.generated.h"
+
+UCLASS()
+class RACINGGAME_API ABrakeAbleGimmick : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABrakeAbleGimmick();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Hyonil)
+	TObjectPtr<class UBoxComponent> UBoxComponent;
+
+};
