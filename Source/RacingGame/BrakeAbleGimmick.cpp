355916e2e3f7b35cbc7e95a01df62c4eb6878d56
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-03-20 21:30:28 +0900

    Collision Enter

--- Source/RacingGame/BrakeAbleGimmick.cpp
+++ Source/RacingGame/BrakeAbleGimmick.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BrakeAbleGimmick.h"
+#include "Components/BoxComponent.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
 
 // Sets default values
@@ -10,20 +11,35 @@ ABrakeAbleGimmick::ABrakeAbleGimmick()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	BoxComponent = GetOwner()->FindComponentByClass<UBoxComponent>();
+	//BoxComponent = GetOwner()->FindComponentByClass<UBoxComponent>();
+	MaxHp = 100;
+	CurrentHp = MaxHp;
+	/*Trigger = Cast<UBoxComponent>(GetOwner()->GetDefaultSubobjectByName(FName(TEXT("Box"))));*/
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	Trigger->SetBoxExtent(FVector(40.0f, 42.0f, 30.0f));
+	RootComponent = Trigger;
 }
 
-// Called when the game starts or when spawned
+// Called when the game starts or when spawned 
 void ABrakeAbleGimmick::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &ABrakeAbleGimmick::OnOverlapBegin);
 }
 
 // Called every frame
 void ABrakeAbleGimmick::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
+void ABrakeAbleGimmick::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
+void ABrakeAbleGimmick::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+	CurrentHp -= 10;
 }
 
--- Source/RacingGame/BrakeAbleGimmick.h
+++ Source/RacingGame/BrakeAbleGimmick.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
 #include "BrakeAbleGimmick.generated.h"
 
 UCLASS()
@@ -27,6 +28,21 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category = Hyonil)
 	TObjectPtr<class UBoxComponent> BoxComponent;
 
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category = Hyonil)
+	float MaxHp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category = Hyonil)
+	float CurrentHp;
+
+
+protected:
+	virtual void PostInitializeComponents() override;
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category = Hyonil)
+	TObjectPtr<UBoxComponent> Trigger;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult);
 //protected:
 //	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
 //	TObjectPtr<class UGeometryCollectionComponent> GeometryComponent;
